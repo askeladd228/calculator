44383aa0f30ecea8a83a45d4f33f239b5132b17b
Nicholas <swaggychoz@gmail.com>
2022-07-29 15:12:50 +0800

    Seperate the value of the first operand and second operand, which derived from the same function

--- script.js
+++ script.js
@@ -14,31 +14,61 @@ function divide(a, b) {
   return a/b;
 };
 
-function operate (op, a, b) {
-  return op(a, b);
+function operate (operator, a, b) {
+  return operator(a, b);
 }
 
 function clear () {
-  bigDisplay.innerHTML = '';
+  bigDisplay.innerHTML = '0';
+  smallDisplay.innerHTML = '';
 }
 
+const display = document.querySelector('.display');
 const bigDisplay = document.querySelector('.bigDisplay');
 const smallDisplay = document.querySelector('.smallDisplay');
 const nums = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.operator');
-const equal = document.querySelector('.equal');
+const equal = document.querySelector('.operate');
+const clearField = document.querySelector('.clear');
 
-let firstOperand = nums.forEach(num => {
+let firstOperand = 0;
+let secondOperand = 0;
+let theOperator = '';
+
+nums.forEach(num => {
   num.addEventListener('click', () => {
-    bigDisplay.innerHTML += num.innerText;
+    if (smallDisplay.innerHTML == '') {
+      let bigDisplayValue = (bigDisplay.innerHTML += num.innerText);
+      firstOperand = bigDisplayValue;
+    };
+    if (smallDisplay.innerHTML !== '') {
+      let bigDisplayValueTwo = (bigDisplay.innerHTML += num.innerText);
+      secondOperand = bigDisplayValueTwo;
+    };
   });
-  return bigDisplay.innerHTML;    
 });
 
-let theOperator = operators.forEach(operator => {
+operators.forEach(operator => {
   operator.addEventListener('click', () => {
-    bigDisplay.innerHTML += operator.innerText;
-  });    
+    smallDisplay.innerHTML = firstOperand + operator.innerText;
+    bigDisplay.innerHTML = '';
+
+    if (operator.innerText == '+') {
+      theOperator = sum;
+    } else if (operator.innerText == '-') {
+      theOperator = substract;
+    } else if (operator.innerText == '×') {
+      theOperator = multiply;
+    } else if (operator.innerText == '÷') {
+      theOperator = divide;
+    };
+  });
 });
+
+equal.addEventListener('click', () => {
+  return bigDisplay.innerHTML = operate(theOperator, parseFloat(firstOperand), parseFloat(secondOperand));
+});
+
+clearField.addEventListener('click', () => clear());
 
 
